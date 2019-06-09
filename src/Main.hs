9af3585c146242029b94c26b5cd5bd4541016ab4
Johannes Drever <jd@linova.de>
2019-06-09 22:28:55 +0200

    Add comments

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,17 @@ module Main where
 import Data.List
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "Hello, Spencer Brown!"
 
+-- | 1
+-- | The form
+
+data Distinction = PerfectContinence
+
+-- | 2
+-- | Froms taken out of the form
+
+-- | Expression
 
 data Expr =
     Unmarked
@@ -19,16 +28,51 @@ instance Show Expr where
 unmarked = Unmarked
 marked = Cross Unmarked
 
+-- | Primitive equation
+
 primitive1 (Call (Cross Unmarked) (Cross Unmarked)) = Call (Cross Unmarked) Unmarked
 primitive1 e = e
 
 primitive2 (Cross (Cross Unmarked)) = Unmarked
 primitive2 e = e
 
+-- | Simple expressions
+
+simpleExpressions = [
+        Call marked marked
+      , Cross marked
+      , Cross Unmarked
+      , Unmarked]
+
+-- | Operation
+cross = Cross
+
+-- | Relation
+
+continence :: Expr -> Expr -> Bool
+continence = undefined
+
+-- | 3
+-- | The conception of calculation
+
+-- | Fourth canon. Hypothesis of simplification
+
 fourthCanon :: Expr
 fourthCanon = Call (Cross (Call (Cross Unmarked) (Cross Unmarked))) (Cross Unmarked)
 fourthCanon' = Call (Cross (Call marked marked)) marked
 
+theorem3 =
+  Call (Cross
+     (Call (Cross
+         (Call
+               (Cross (Call marked marked))
+               (marked)))
+        (Cross marked)))
+    (Cross (Cross (Cross marked)))
 
 data AlgExpr = AlgExpr
+
+-- | Appendix 2
+-- | The calculus interpreted as logic
+
 
