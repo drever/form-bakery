e8f5b4f80af7b1f12f3a7f5cde25b5ab7163c5d6
Johannes Drever <jd@linova.de>
2019-06-14 14:23:50 +0200

    Formating

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -12,12 +12,21 @@ import Common
 import Markup
 
 main :: IO ()
-main = mainWidgetWithCss css  $ el "div" $ do
-  t <-  inputElement $ def
-       & inputElementConfig_initialValue .~ (T.pack . show $ theorem3)
-  dyn $ (fmap ((\x -> case x of
-                        Just x' -> showM x'
-                        Nothing -> text "???") . fst . parseExpr . T.unpack)) $ _inputElement_value t
+main = mainWidgetWithCss css  $ do
+  heading
+  el "div" parseAndRenderWidget
+  return ()
 
-  el "p" $ text "An invitation to the Laws of Form"
    where css = $(embedFile "css/mark.css")
+
+heading :: DomBuilder t m => m ()
+heading =
+  el "h1" $ text "An invitation to the Laws of Form"
+
+parseAndRenderWidget :: (DomBuilder t m, PostBuild t m) => m (Event t ())
+parseAndRenderWidget = do
+      t <-  inputElement $ def
+           & inputElementConfig_initialValue .~ (T.pack . show $ theorem3)
+      dyn $ (fmap ((\x -> case x of
+                            Just x' -> showM x'
+                            Nothing -> text "???") . fst . parseExpr . T.unpack)) $ _inputElement_value t
